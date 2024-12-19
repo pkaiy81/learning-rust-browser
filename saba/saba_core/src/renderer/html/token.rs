445d3f6f005e8cfdb57b53324c95911f591e9e74
k8k1 <k.kohei_h_pc@outlook.jp>
2024-12-19 22:58:47 +0900

    [chap-4] Create state machine.

---
+++ saba/saba_core/src/renderer/html/token.rs
@@ -0,0 +1,297 @@
+use crate::renderer::html::attribute::Attribute;
+use alloc::string::String;
+use alloc::vec::Vec;
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub enum HtmlToken {
+    // Start tag token
+    StartTag {
+        tag: String,
+        self_closing: bool,
+        attributes: Vec<Attribute>,
+    },
+    // End tag token
+    EndTag {
+        tag: String,
+    },
+    // String token
+    Char(char),
+    // End of file
+    Eof,
+}
+
+// 13.2.5 Tokenization
+// There are 80 states in the HTML specification.
+// But I will implement only the states that are necessary for the rendering engine.
+// TemporaryBuffer is not a state, but it is used in the specification.
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub enum State {
+    /// https://html.spec.whatwg.org/multipage/parsing.html#data-state
+    Data,
+    /// https://html.spec.whatwg.org/multipage/parsing.html#tag-open-state
+    TagOpen,
+    /// https://html.spec.whatwg.org/multipage/parsing.html#end-tag-open-state
+    EndTagOpen,
+    /// https://html.spec.whatwg.org/multipage/parsing.html#tag-name-state
+    TagName,
+    /// https://html.spec.whatwg.org/multipage/parsing.html#before-attribute-name-state
+    BeforeAttributeName,
+    /// https://html.spec.whatwg.org/multipage/parsing.html#attribute-name-state
+    AttributeName,
+    /// https://html.spec.whatwg.org/multipage/parsing.html#after-attribute-name-state
+    AfterAttributeName,
+    /// https://html.spec.whatwg.org/multipage/parsing.html#before-attribute-value-state
+    BeforeAttributeValue,
+    /// https://html.spec.whatwg.org/multipage/parsing.html#attribute-value-(double-quoted)-state
+    AttributeValueDoubleQuoted,
+    /// https://html.spec.whatwg.org/multipage/parsing.html#attribute-value-(single-quoted)-state
+    AttributeValueSingleQuoted,
+    /// https://html.spec.whatwg.org/multipage/parsing.html#attribute-value-(unquoted)-state
+    AttributeValueUnquoted,
+    /// https://html.spec.whatwg.org/multipage/parsing.html#after-attribute-value-(quoted)-state
+    AfterAttributeValueQuoted,
+    /// https://html.spec.whatwg.org/multipage/parsing.html#self-closing-start-tag-state
+    SelfClosingStartTag,
+    /// https://html.spec.whatwg.org/multipage/parsing.html#script-data-state
+    ScriptData,
+    /// https://html.spec.whatwg.org/multipage/parsing.html#script-data-less-than-sign-state
+    ScriptDataLessThanSign,
+    /// https://html.spec.whatwg.org/multipage/parsing.html#script-data-end-tag-open-state
+    ScriptDataEndTagOpen,
+    /// https://html.spec.whatwg.org/multipage/parsing.html#script-data-end-tag-name-state
+    ScriptDataEndTagName,
+    /// https://html.spec.whatwg.org/multipage/parsing.html#temporary-buffer
+    TemporaryBuffer,
+}
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub struct HtmlTokenizer {
+    state: State,
+    pos: usize,
+    reconsume: bool,
+    latest_token: Option<HtmlToken>,
+    input: Vec<char>,
+    buf: String,
+}
+
+impl HtmlTokenizer {
+    pub fn new(html: String) -> Self {
+        Self {
+            state: State::Data,
+            pos: 0,
+            reconsume: false,
+            latest_token: None,
+            input: html.chars().collect(),
+            buf: String::new(),
+        }
+    }
+
+    // Check if the current position is the end of the file
+    fn is_eof(&self) -> bool {
+        self.pos > self.input.len()
+    }
+
+    // Current position moved to the next character
+    fn consume_next_input(&mut self) -> char {
+        let c = self.input[self.pos];
+        self.pos += 1;
+        c
+    }
+
+    // Create a tag
+    // If start_tag_token is true, create a StartTag token
+    // Otherwise, create an EndTag token
+    fn create_tag(&mut self, start_tag_token: bool) {
+        if start_tag_token {
+            self.latest_token = Some(HtmlToken::StartTag {
+                tag: String::new(),
+                self_closing: false,
+                attributes: Vec::new(),
+            });
+        } else {
+            self.latest_token = Some(HtmlToken::EndTag { tag: String::new() })
+        }
+    }
+
+    // Set reconsume flag to false
+    // Return the character before the current position
+    fn reconsume_input(&mut self) -> char {
+        self.reconsume = false;
+        self.input[self.pos - 1]
+    }
+
+    // Append the character to the tag name in the latest_token created by create_tag
+    fn append_tag_name(&mut self, c: char) {
+        assert!(self.latest_token.is_some());
+
+        if let Some(t) = self.latest_token.as_mut() {
+            match t {
+                HtmlToken::StartTag {
+                    ref mut tag,
+                    self_closing: _, // Ignore self_closing
+                    attributes: _,   // Ignore attributes
+                }
+                | HtmlToken::EndTag { ref mut tag } => tag.push(c),
+                _ => panic!("`latest_token` should be either StartTag or EndTag"),
+            }
+        }
+    }
+
+    // Take the latest_token and return it
+    fn take_latest_token(&mut self) -> Option<HtmlToken> {
+        assert!(self.latest_token.is_some());
+
+        let t = self.latest_token.as_ref().cloned();
+        self.latest_token = None;
+        assert!(self.latest_token.is_none());
+
+        t
+    }
+
+    // Start a new attribute
+    // Create a new Attribute and append it to the latest_token
+    fn start_new_attribute(&mut self) {
+        assert!(self.latest_token.is_some());
+
+        if let Some(t) = self.latest_token.as_mut() {
+            match t {
+                HtmlToken::StartTag {
+                    tag: _,
+                    self_closing: _, // Ignore self_closing
+                    ref mut attributes,
+                } => {
+                    attributes.push(Attribute::new());
+                }
+                _ => panic!("`latest_token` should be either StartTag"),
+            }
+        }
+    }
+}
+
+// Implement the Iterator trait for HtmlTokenizer
+// https://doc.rust-lang.org/std/iter/trait.Iterator.html
+impl Iterator for HtmlTokenizer {
+    type Item = HtmlToken;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.pos >= self.input.len() {
+            return None;
+        }
+
+        loop {
+            let c = match self.reconsume {
+                true => self.reconsume_input(),
+                false => self.consume_next_input(),
+            };
+
+            // Implement the states here
+            match self.state {
+                State::Data => {
+                    // If character is '<', switch to TagOpen state
+                    // Also, if character is EOF, return Eof token
+                    // Otherwise, return Char token
+                    if c == '<' {
+                        self.state = State::TagOpen;
+                        continue;
+                    }
+
+                    if self.is_eof() {
+                        return Some(HtmlToken::Eof);
+                    }
+
+                    return Some(HtmlToken::Char(c));
+                }
+
+                State::TagOpen => {
+                    // If character is '/', switch to EndTagOpen state
+                    // If character is an ASCII letter, switch to TagName state
+                    // Otherwise, return Char token
+                    if c == '/' {
+                        self.state = State::EndTagOpen;
+                        continue;
+                    }
+
+                    if c.is_ascii_alphabetic() {
+                        self.reconsume = true;
+                        self.state = State::TagName;
+                        self.create_tag(true);
+                        continue;
+                    }
+
+                    if self.is_eof() {
+                        return Some(HtmlToken::Eof);
+                    }
+
+                    self.reconsume = true;
+                    self.state = State::Data;
+                }
+
+                // EndTagOpen state handles end tags.
+                // If character achieves the end of file, return Eof token.
+                // If character is an ASCII letter, switch to TagName state
+                State::EndTagOpen => {
+                    if self.is_eof() {
+                        return Some(HtmlToken::Eof);
+                    }
+
+                    if c.is_ascii_alphabetic() {
+                        self.reconsume = true;
+                        self.state = State::TagName;
+                        self.create_tag(false);
+                        continue;
+                    }
+                }
+
+                // TagName state handles tag names.
+                // 1. If character is a whitespace character, switch to BeforeAttributeName state
+                // 2. If character is '/', switch to SelfClosingStartTag state
+                // 3. If character is '>', switch to Data state and return the latest_token created by create_tag
+                // 4. If character is an ASCII alpha, append the character to the tag name
+                // 5. IF character achieves the end of file, return Eof token
+                // 6. Otherwise, append the character to the tag name
+                State::TagName => {
+                    if c == ' ' {
+                        self.state = State::BeforeAttributeName;
+                        continue;
+                    }
+
+                    if c == '/' {
+                        self.state = State::SelfClosingStartTag;
+                        continue;
+                    }
+
+                    if c == '>' {
+                        self.state = State::Data;
+                        return self.take_latest_token();
+                    }
+
+                    if c.is_ascii_uppercase() {
+                        self.append_tag_name(c.to_ascii_lowercase());
+                        continue;
+                    }
+
+                    if self.is_eof() {
+                        return Some(HtmlToken::Eof);
+                    }
+
+                    self.append_tag_name(c);
+                }
+
+                // BeforeAttributeName state handles attributes.
+                // 1. If character is '/' or '>' or EOF, set the reconsume flag to true and switch to AfterAttributeName state.
+                // 2. Otherwise, switch to AttributeName state and call start_new_attribute method.
+                State::BeforeAttributeName => {
+                    if c == '/' || c == '>' || self.is_eof() {
+                        self.reconsume = true;
+                        self.state = State::AfterAttributeName;
+                        continue;
+                    }
+
+                    self.reconsume = true;
+                    self.state = State::AttributeName;
+                    self.start_new_attribute();
+                }
+            }
+        }
+    }
+}

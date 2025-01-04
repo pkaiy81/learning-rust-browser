6157e2383fa8aa6585a3c2d81a8557da23d5b50c
k8k1 <k.kohei_h_pc@outlook.jp>
2025-01-04 19:18:44 +0900

    [chap-5] ~p.214 double/single quote.

---
+++ saba/saba_core/src/renderer/css/token.rs
@@ -0,0 +1,106 @@
+use alloc::string::String;
+use alloc::vec::Vec;
+
+#[derive(Debug, Clone, PartialEq)]
+pub enum CssToken {
+    /// https://www.w3.org/TR/css-syntax-3/#typedef-hash-token
+    HashToken(String),
+    /// https://www.w3.org/TR/css-syntax-3/#typedef-delim-token
+    Delim(char),
+    /// https://www.w3.org/TR/css-syntax-3/#typedef-number-token
+    Number(f64),
+    /// https://www.w3.org/TR/css-syntax-3/#typedef-colon-token
+    Colon,
+    /// https://www.w3.org/TR/css-syntax-3/#typedef-semicolon-token
+    SemiColon,
+    /// https://www.w3.org/TR/css-syntax-3/#tokendef-open-paren
+    OpenParenthesis,
+    /// https://www.w3.org/TR/css-syntax-3/#tokendef-close-paren
+    CloseParenthesis,
+    /// https://www.w3.org/TR/css-syntax-3/#tokendef-open-curly
+    OpenCurly,
+    /// https://www.w3.org/TR/css-syntax-3/#tokendef-close-curly
+    CloseCurly,
+    /// https://www.w3.org/TR/css-syntax-3/#typedef-ident-token
+    Ident(String),
+    /// https://www.w3.org/TR/css-syntax-3/#typedef-string-token
+    StringToken(String),
+    /// https://www.w3.org/TR/css-syntax-3/#typedef-at-keyword-token
+    AtKeyword(String),
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub struct CssTokenizer {
+    pos: usize,
+    input: Vec<char>,
+}
+
+impl CssTokenizer {
+    pub fn new(css: String) -> Self {
+        Self {
+            pos: 0,
+            input: css.chars().collect(),
+        }
+    }
+
+    // https://www.w3.org/TR/css-syntax-3/#consume-string-token
+    fn consume_string_token(&mut self) -> String {
+        let mut s = String::new();
+
+        loop {
+            if self.pos >= self.input.len() {
+                return s;
+            }
+
+            self.pos += 1;
+            let c = self.input[self.pos];
+            match c {
+                '"' | '\'' => break,
+                _ => s.push(c),
+            }
+        }
+
+        s
+    }
+}
+
+impl Iterator for CssTokenizer {
+    type Item = CssToken;
+
+    /// https://www.w3.org/TR/css-syntax-3/#consume-token
+    fn next(&mut self) -> Option<Self::Item> {
+        loop {
+            if self.pos >= self.input.len() {
+                return None;
+            }
+
+            let c = self.input[self.pos];
+
+            let token = match c {
+                // Determine next token
+                '(' => CssToken::OpenParenthesis,
+                ')' => CssToken::CloseParenthesis,
+                ',' => CssToken::Delim(','),
+                '.' => CssToken::Delim('.'),
+                ':' => CssToken::Colon,
+                ';' => CssToken::SemiColon,
+                '{' => CssToken::OpenCurly,
+                '}' => CssToken::CloseCurly,
+                ' ' | '\n' => {
+                    self.pos += 1;
+                    continue;
+                }
+                '"' | '\'' => {
+                    let value = self.consume_string_token();
+                    CssToken::StringToken(value)
+                }
+                _ => {
+                    unimplemented!("char {} is not implemented yet", c);
+                }
+            };
+
+            self.pos += 1;
+            return Some(token);
+        }
+    }
+}
